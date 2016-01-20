7e24ade16f954519c71c763a0ec545bc13b92ec3
iGerrebrands <i.gerrebrands@mondriaanict.nl>
2016-01-20 12:38:28 +0100

    Edited content

--- js/ctrl/myProjectController.js
+++ js/ctrl/myProjectController.js
@@ -1,7 +1,9 @@
 myPortfolioApp.controller('myProjectCtrl', function($scope) {
     $scope.projects = [
         {name:"Techniek Innovatiehuis", date:["Nov 2015","Dec 2015"], img:"image/techniekinnovatiehuis.png",
-            href:"http://www.techniekinnovatiehuis.nl", desc:"Some bullshit"},
+            href:"http://www.techniekinnovatiehuis.nl", desc:"We did create a website for the Techniek Innovatiehuis project, this is a project Roc Mondriaan is working on. " +
+                "They want to inspire people to work together on projects. Techniek Innovatiehuis is going to make that possible. " +
+                "We had the job to create the website and the CMS (Content Management System). They are using this website for publicity."},
 
         {name:"Example project1", date:["Nov 2015","Dec 2015"], img:"image/project-placeholder.jpg",
             href:"http://www.techniekinnovatiehuis.nl", desc:"Some bullshit"},
