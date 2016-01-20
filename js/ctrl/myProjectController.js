c9aa3c2f52d07778fd765aefc08c52d011b8fec9
iGerrebrands <i.gerrebrands@mondriaanict.nl>
2016-01-20 11:36:46 +0100

    Angular Done waiting for testing

--- js/ctrl/myProjectController.js
+++ js/ctrl/myProjectController.js
@@ -1,9 +1,12 @@
 myPortfolioApp.controller('myProjectCtrl', function($scope) {
     $scope.projects = [
-        {name:"Techniek Innovatiehuis", date:["Nov 2015","Dec 2015"], img:"images/techniekinnovatiehuis.png",
+        {name:"Techniek Innovatiehuis", date:["Nov 2015","Dec 2015"], img:"image/techniekinnovatiehuis.png",
             href:"http://www.techniekinnovatiehuis.nl", desc:"Some bullshit"},
 
-        {name:"Example project1", date:["Nov 2015","Dec 2015"], img:"images/project-placeholder.jpg",
-            href:"http://www.techniekinnovatiehuis.nl", desc:"Some bullshit"}
+        {name:"Example project1", date:["Nov 2015","Dec 2015"], img:"image/project-placeholder.jpg",
+            href:"http://www.techniekinnovatiehuis.nl", desc:"Some bullshit"},
+
+        {name:"Example project2", date:["Nov 2015", "Dec 2015"], img:"image/project-placeholder2.jpg",
+            href:"http://www.google.nl", desc:"Some bullshit"}
     ];
 });
