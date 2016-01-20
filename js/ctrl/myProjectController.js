965da34435435d555b99ad2522aef4424d774022
iGerrebrands <i.gerrebrands@mondriaanict.nl>
2016-01-20 16:32:45 +0100

    Filled in some project content

--- js/ctrl/myProjectController.js
+++ js/ctrl/myProjectController.js
@@ -5,10 +5,9 @@ myPortfolioApp.controller('myProjectCtrl', function($scope) {
                 "They want to inspire people to work together on projects. Techniek Innovatiehuis is going to make that possible. " +
                 "We had the job to create the website and the CMS (Content Management System). They are using this website for publicity."},
 
-        {name:"Example project1", date:["Nov 2015","Dec 2015"], img:"image/project-placeholder.jpg",
-            href:"http://www.techniekinnovatiehuis.nl", desc:"A description."},
-
-        {name:"Example project2", date:["Nov 2015", "Dec 2015"], img:"image/project-placeholder2.jpg",
-            href:"http://www.google.nl", desc:"A description."}
+        {name:"Android schedule app", date:["Jan 2015","Mar 2015"], img:"image/project-rooster-app.jpg",
+            href:"", desc:"A classmate and I developed a schedule app for android, it was our school project. " +
+                    "I made half of the front-end my classmate made the half of the front-end and all of the back-end. " +
+                    "In the app you get a overview of your schedule and you can see an overview of all the hours you are absent or present."},
     ];
 });
