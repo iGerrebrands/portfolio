71e08ed265b6394dbf7f5c765a4db5aa36689b75
iGerrebrands <i.gerrebrands@mondriaanict.nl>
2016-01-22 17:45:50 +0100

    bug fix

---
+++ js/nav/navService.js
@@ -0,0 +1,26 @@
+portfolioApp
+    .service('navigationService', function(
+        NAVIGATION
+    ){
+        //this.toggleMinimizerState = function() {
+        //    var minimizer = NAVIGATION.minimizer;
+        //    if(!minimizer.minimized){
+        //        minimizer.state = minimizer.state === minimizer.min.text ? minimizer.min.desc : minimizer.min.text;
+        //    } else {
+        //        minimizer.state = minimizer.state === minimizer.max.text ? minimizer.max.desc : minimizer.max.text;
+        //    }
+        //};
+        //
+        //this.toggleMinimizerArticles = function() {
+        //    var minimizer = NAVIGATION.minimizer;
+        //    //if not minimized minimize all articles
+        //    if(!minimizer.minimized){
+        //        minimize();
+        //    } else {
+        //        maximize();
+        //    }
+        //    minimizer.minimized = !minimizer.minimized;
+        //    //setting the state so the text will change when clicked
+        //    minimizer.state = minimizer.minimized ? minimizer.max.desc : minimizer.min.desc;
+        //};
+    });
