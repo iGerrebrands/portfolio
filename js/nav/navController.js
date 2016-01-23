9b697d380b33f06b491ca39e81dd7017d6f53663
= <=>
2016-01-23 20:08:20 +0100

    Replaced some code

--- js/nav/navController.js
+++ js/nav/navController.js
@@ -2,19 +2,14 @@
     portfolioApp
         .controller('navCtrl', function(
             $scope,
-            NAVIGATION
+            NAVIGATION,
+            navigationService
         ){
             $scope.nav = NAVIGATION;
             $scope.boolChangeClass = false;
 
-            //Makes the link where is clicked on active (makes it red)
             $scope.activate = function(element){
-                _.each(NAVIGATION.links, function(link) {
-                    if(link.active) link.active = false;
-                    if(element === link){
-                        link.active = true;
-                    }
-                });
-            };
+                navigationService.activate(element);
+            }
         });
 })();
--- js/nav/navService.js
+++ js/nav/navService.js
@@ -2,25 +2,13 @@ portfolioApp
     .service('navigationService', function(
         NAVIGATION
     ){
-        //this.toggleMinimizerState = function() {
-        //    var minimizer = NAVIGATION.minimizer;
-        //    if(!minimizer.minimized){
-        //        minimizer.state = minimizer.state === minimizer.min.text ? minimizer.min.desc : minimizer.min.text;
-        //    } else {
-        //        minimizer.state = minimizer.state === minimizer.max.text ? minimizer.max.desc : minimizer.max.text;
-        //    }
-        //};
-        //
-        //this.toggleMinimizerArticles = function() {
-        //    var minimizer = NAVIGATION.minimizer;
-        //    //if not minimized minimize all articles
-        //    if(!minimizer.minimized){
-        //        minimize();
-        //    } else {
-        //        maximize();
-        //    }
-        //    minimizer.minimized = !minimizer.minimized;
-        //    //setting the state so the text will change when clicked
-        //    minimizer.state = minimizer.minimized ? minimizer.max.desc : minimizer.min.desc;
-        //};
+        //Makes the link where is clicked on active (makes it red)
+       this.activate = function(element){
+           _.each(NAVIGATION.links, function(link) {
+               if(link.active) link.active = false;
+               if(element === link){
+                   link.active = true;
+               }
+           });
+       };
     });
